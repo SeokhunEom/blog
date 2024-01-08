333fccf9437a0775ebc0a5fc2f26ea0d2a484aa4
tommya98 <tommya98@naver.com>
2024-01-09 00:32:57 +0900

    FEAT: 홈 화면 이미지 추가

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>주니어 개발자의 개발 블로그입니다.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/icon.png"
+      />
     </Layout>
   );
 };
