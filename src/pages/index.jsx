492e2cfab3ac7b4f0d57ea9393a7794dd8ef644a
tommya98 <tommya98@naver.com>
2024-01-08 22:44:47 +0900

    FEAT: Home 페이지 기초 구현

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>SeokHun's Dev Blog</h1>
+      <p>주니어 개발자의 개발 블로그입니다.</p>
+    </main>
+  );
+};
+
+export const Head = () => <title>Home</title>;
+
+export default IndexPage;
