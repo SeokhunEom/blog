3e44f2d69999c1a741de4d8c7e7875cca9255f12
tommya98 <tommya98@naver.com>
2024-01-15 17:26:03 +0900

    FEAT: 블로그 포스트별 동적 페이지 생성 구현

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,16 +1,19 @@
 import * as React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
+import { graphql, Link } from "gatsby";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.frontmatter.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          </h2>
           <p>작성일: {node.frontmatter.date}</p>
-          <p>{node.excerpt}</p>
         </article>
       ))}
     </Layout>
@@ -24,6 +27,7 @@ export const query = graphql`
         frontmatter {
           date(formatString: "YYYY.MM.DD")
           title
+          slug
         }
         id
         excerpt
