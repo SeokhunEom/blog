c4d646a8082ff9eee659809d831bad6c486f3e1b
tommya98 <tommya98@naver.com>
2024-01-22 17:29:08 +0900

    REFACTOR: useTocScroll 리팩토링

--- src/hooks/useTocScroll.ts
+++ src/hooks/useTocScroll.ts
@@ -1,59 +1,44 @@
 import { useEffect, useRef } from "react";
 
+const OFFSET = 60;
+
 const useTocScroll = () => {
   const contentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const contentElement = contentRef.current;
-    if (contentElement === null) return;
+    if (!contentElement) return;
 
     const handleClick = (e: MouseEvent) => {
-      const target = e.currentTarget as HTMLElement;
-      if (target.tagName !== "A") return;
-      const targetHref = target.getAttribute("href") as string;
-      if (targetHref.startsWith("#fn-")) return;
-      if (!targetHref.startsWith("#")) return;
+      const clickedElement = e.currentTarget as HTMLElement;
+      if (clickedElement.tagName !== "A") return;
 
-      if (targetHref.startsWith("#fnref-")) {
+      const href = clickedElement.getAttribute("href");
+      if (!href || !href.startsWith("#") || href.startsWith("#fn-")) return;
+      const adjustedHref = href.startsWith("#fnref-")
+        ? href.replace("ref", "")
+        : href;
+      const targetAnchor = Array.from(
+        contentElement.querySelectorAll("a")
+      ).find(
+        (anchor) => anchor.getAttribute("href") === adjustedHref
+      ) as HTMLAnchorElement;
+
+      if (targetAnchor) {
         e.preventDefault();
-        if (target) {
-          const target = Array.from(
-            contentElement.querySelectorAll("a")
-          ).filter((anchor) =>
-            anchor.getAttribute("href")?.includes(targetHref.replace("ref", ""))
-          )[0] as HTMLAnchorElement;
-          console.log(target);
-          if (target) {
-            const targetPosition =
-              target.getBoundingClientRect().top + window.scrollY;
-            window.scrollTo({ top: targetPosition - 60, behavior: "smooth" });
-          }
-        }
-        return;
-      }
-
-      e.preventDefault();
-      if (target) {
-        const target = Array.from(contentElement.querySelectorAll("a")).filter(
-          (anchor) => anchor.getAttribute("href")?.includes(targetHref)
-        )[0] as HTMLAnchorElement;
-        if (target) {
-          const targetPosition =
-            target.getBoundingClientRect().top + window.scrollY;
-          window.scrollTo({ top: targetPosition - 60, behavior: "smooth" });
-        }
+        const targetPosition =
+          targetAnchor.getBoundingClientRect().top + window.scrollY;
+        window.scrollTo({ top: targetPosition - OFFSET, behavior: "smooth" });
       }
     };
 
     const anchors = contentElement.querySelectorAll("a");
-    anchors.forEach((anchor) => {
-      anchor.addEventListener("click", handleClick);
-    });
+    anchors.forEach((anchor) => anchor.addEventListener("click", handleClick));
 
     return () => {
-      anchors.forEach((anchor) => {
-        anchor.removeEventListener("click", handleClick);
-      });
+      anchors.forEach((anchor) =>
+        anchor.removeEventListener("click", handleClick)
+      );
     };
   }, []);
 
